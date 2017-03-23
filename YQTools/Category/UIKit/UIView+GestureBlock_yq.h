bd8b5247bc658521e0df55ca6556963f8af274e8
weixb <weixinbing@17m.club>
2017-03-23 18:28:06 +0800

    封装常用手势block

--- YQTools/Category/UIKit/UIView+GestureBlock_yq.h
+++ YQTools/Category/UIKit/UIView+GestureBlock_yq.h
@@ -11,25 +11,70 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface UIView (GestureBlock_yq)
-
 /**
- *  @brief  添加单击手势
+ *  轻击手势
+ *
+ *  @param numberOfTouches 触摸手指数
+ *  @param numberOfTaps    点击次数
+ *  @param block           回调
+ */
+- (void)yq_whenTouches:(NSUInteger)numberOfTouches taps:(NSUInteger)numberOfTaps block:(void(^)(UITapGestureRecognizer *tapGesture))block;
+/**
+ *  @brief  单击手势
  *
  *  @param block 回调
  */
 - (void)yq_whenTapped:(void(^)(UITapGestureRecognizer *tapGesture))block;
 /**
- *  @brief  添加双击手势
+ *  @brief  双击手势
  *
  *  @param block 回调
  */
 - (void)yq_whenDoubleTapped:(void(^)(UITapGestureRecognizer *tapGesture))block;
+
 /**
- *  @brief  添加长按手势
+ *  @brief  长按手势
  *
  *  @param block 回调
  */
 - (void)yq_whenLongPressed:(void(^)(UILongPressGestureRecognizer *longPressGesture))block;
+
+/**
+ *  轻扫手势
+ *
+ *  @param numberOfTouches 触摸手指数
+ *  @param direction       轻扫方向
+ *  @param block           回调
+ */
+- (void)yq_whenSwipedWithTouches:(NSUInteger)numberOfTouches direction:(UISwipeGestureRecognizerDirection)direction block:(void(^)(UISwipeGestureRecognizer *swipeGesture))block;
+/**
+ *  @brief  单指轻扫手势
+ *
+ *  @param block 回调
+ */
+- (void)yq_whenSwipedWithDirection:(UISwipeGestureRecognizerDirection)direction block:(void(^)(UISwipeGestureRecognizer *swipeGesture))block;
+
+/**
+ *  @brief  捏合手势
+ *
+ *  @param block 回调
+ */
+- (void)yq_whenPinch:(void(^)(UIPinchGestureRecognizer *pinchGesture))block;
+
+/**
+ *  @brief  拖动手势
+ *
+ *  @param block 回调
+ */
+- (void)yq_whenPan:(void(^)(UIPanGestureRecognizer *pinchGesture))block;
+
+/**
+ *  @brief  旋转手势
+ *
+ *  @param block 回调
+ */
+- (void)yq_whenRotation:(void(^)(UIRotationGestureRecognizer *pinchGesture))block;
+
 
 @end
 
--- YQTools/Category/UIKit/UIView+GestureBlock_yq.m
+++ YQTools/Category/UIKit/UIView+GestureBlock_yq.m
@@ -8,73 +8,57 @@
 
 #import "UIView+GestureBlock_yq.h"
 @import ObjectiveC.runtime;
-/* 单击key */
-static char yq_kActionHandlerTapBlockKey;
-static char yq_kActionHandlerTapGestureKey;
-/* 双击key */
-static char yq_kActionHandlerDoubleTapBlockKey;
-static char yq_kActionHandlerDoubleTapGestureKey;
-/* 长按key */
-static char yq_kActionHandlerLongPressBlockKey;
-static char yq_kActionHandlerLongPressGestureKey;
+
+#define yq_objcAssociatedGesturekKey (__bridge const void *)(block)
+#define yq_objcAssociatedBlockkKey (__bridge const void *)(gesture)
 
 @implementation UIView (GestureBlock_yq)
 
-- (void)yq_whenTapped:(void (^)(UITapGestureRecognizer * _Nonnull))block {
-    UITapGestureRecognizer *gesture = objc_getAssociatedObject(self, &yq_kActionHandlerTapGestureKey);
+/* 轻击手势key */
+//static char yq_kActionHandlerTapBlockKey;
+
+- (void)yq_whenTouches:(NSUInteger)numberOfTouches taps:(NSUInteger)numberOfTaps block:(void (^)(UITapGestureRecognizer * _Nonnull))block {
+    UITapGestureRecognizer *gesture = objc_getAssociatedObject(self, yq_objcAssociatedGesturekKey);
     if (!gesture)
     {
         gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(yq_handleActionForTapGesture:)];
+        gesture.numberOfTouchesRequired = numberOfTouches; //触摸手指数
+        gesture.numberOfTapsRequired = numberOfTaps; //点击次数
         [self addGestureRecognizer:gesture];
-        objc_setAssociatedObject(self, &yq_kActionHandlerTapGestureKey, gesture, OBJC_ASSOCIATION_RETAIN);
+        objc_setAssociatedObject(self, yq_objcAssociatedGesturekKey, gesture, OBJC_ASSOCIATION_RETAIN);
     }
-    objc_setAssociatedObject(self, &yq_kActionHandlerTapBlockKey, block, OBJC_ASSOCIATION_COPY);
+    objc_setAssociatedObject(self, yq_objcAssociatedBlockkKey, block, OBJC_ASSOCIATION_COPY);
 }
 
 - (void)yq_handleActionForTapGesture:(UITapGestureRecognizer*)gesture
 {
     if (gesture.state == UIGestureRecognizerStateRecognized)
     {
-        void(^block)(UITapGestureRecognizer *) = objc_getAssociatedObject(self, &yq_kActionHandlerTapBlockKey);
-        if (block)
-        {
-            block(gesture);
-        }
+        void(^block)(UITapGestureRecognizer *) = objc_getAssociatedObject(self, yq_objcAssociatedBlockkKey);
+        if (block) block(gesture);
     }
-}
-- (void)yq_whenDoubleTapped:(void (^)(UITapGestureRecognizer * _Nonnull))block {
-    UITapGestureRecognizer *gesture = objc_getAssociatedObject(self, &yq_kActionHandlerDoubleTapGestureKey);
-    if (!gesture)
-    {
-        gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(yq_handleActionForDoubleTapGesture:)];
-        gesture.numberOfTouchesRequired = 1; //点击手指数
-        gesture.numberOfTapsRequired = 2; //点击次数
-        [self addGestureRecognizer:gesture];
-        objc_setAssociatedObject(self, &yq_kActionHandlerDoubleTapGestureKey, gesture, OBJC_ASSOCIATION_RETAIN);
-    }
-    objc_setAssociatedObject(self, &yq_kActionHandlerDoubleTapBlockKey, block, OBJC_ASSOCIATION_COPY);
 }
 
-- (void)yq_handleActionForDoubleTapGesture:(UITapGestureRecognizer*)gesture
-{
-    if (gesture.state == UIGestureRecognizerStateRecognized)
-    {
-        void(^block)(UITapGestureRecognizer *) = objc_getAssociatedObject(self, &yq_kActionHandlerDoubleTapBlockKey);
-        if (block)
-        {
-            block(gesture);
-        }
-    }
+/* 单击 */
+- (void)yq_whenTapped:(void (^)(UITapGestureRecognizer * _Nonnull))block {
+    [self yq_whenTouches:1 taps:1 block:block];
 }
+/* 双击 */
+- (void)yq_whenDoubleTapped:(void (^)(UITapGestureRecognizer * _Nonnull))block {
+    [self yq_whenTouches:1 taps:2 block:block];
+}
+
+/* 长按手势key */
+static char yq_kActionHandlerLongPressBlockKey;
 
 - (void)yq_whenLongPressed:(void (^)(UILongPressGestureRecognizer * _Nonnull))block
 {
-    UILongPressGestureRecognizer *gesture = objc_getAssociatedObject(self, &yq_kActionHandlerLongPressGestureKey);
+    UILongPressGestureRecognizer *gesture = objc_getAssociatedObject(self, yq_objcAssociatedGesturekKey);
     if (!gesture)
     {
         gesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(yq_handleActionForLongPressGesture:)];
         [self addGestureRecognizer:gesture];
-        objc_setAssociatedObject(self, &yq_kActionHandlerLongPressGestureKey, gesture, OBJC_ASSOCIATION_RETAIN);
+        objc_setAssociatedObject(self, yq_objcAssociatedGesturekKey, gesture, OBJC_ASSOCIATION_RETAIN);
     }
     objc_setAssociatedObject(self, &yq_kActionHandlerLongPressBlockKey, block, OBJC_ASSOCIATION_COPY);
 }
@@ -83,11 +67,102 @@ static char yq_kActionHandlerLongPressGestureKey;
     if (gesture.state == UIGestureRecognizerStateRecognized)
     {
         void(^block)(UILongPressGestureRecognizer *) = objc_getAssociatedObject(self, &yq_kActionHandlerLongPressBlockKey);
-        if (block)
-        {
-            block(gesture);
-        }
+        if (block) block(gesture);
     }
 }
 
+/* 轻扫手势key */
+//static char yq_kActionHandlerSwipeBlockKey;
+
+- (void)yq_whenSwipedWithTouches:(NSUInteger)numberOfTouches direction:(UISwipeGestureRecognizerDirection)direction block:(void (^)(UISwipeGestureRecognizer * _Nonnull))block {
+    UISwipeGestureRecognizer *gesture = objc_getAssociatedObject(self, yq_objcAssociatedGesturekKey);
+    if (!gesture)
+    {
+        gesture = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(yq_handleActionForSwipeGesture:)];
+        gesture.numberOfTouchesRequired = numberOfTouches; //触摸手指数
+        gesture.direction = direction; //轻扫方向
+        [self addGestureRecognizer:gesture];
+        objc_setAssociatedObject(self, yq_objcAssociatedGesturekKey, gesture, OBJC_ASSOCIATION_RETAIN);
+    }
+    objc_setAssociatedObject(self, yq_objcAssociatedBlockkKey, block, OBJC_ASSOCIATION_COPY);
+}
+/* 单指轻扫 */
+- (void)yq_whenSwipedWithDirection:(UISwipeGestureRecognizerDirection)direction block:(void (^)(UISwipeGestureRecognizer * _Nonnull))block {
+    [self yq_whenSwipedWithTouches:1 direction:direction block:block];
+}
+
+- (void)yq_handleActionForSwipeGesture:(UISwipeGestureRecognizer*)gesture
+{
+    if (gesture.state == UIGestureRecognizerStateRecognized)
+    {
+        void(^block)(UISwipeGestureRecognizer *) = objc_getAssociatedObject(self, yq_objcAssociatedBlockkKey);
+        if (block) block(gesture);
+    }
+}
+
+/* 捏合手势key */
+static char yq_kActionHandlerPinchBlockKey;
+
+- (void)yq_whenPinch:(void (^)(UIPinchGestureRecognizer * _Nonnull))block {
+    UIPinchGestureRecognizer *gesture = objc_getAssociatedObject(self, yq_objcAssociatedGesturekKey);
+    if (!gesture)
+    {
+        gesture = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(yq_handleActionForPinchGesture:)];
+        [self addGestureRecognizer:gesture];
+        objc_setAssociatedObject(self, yq_objcAssociatedGesturekKey, gesture, OBJC_ASSOCIATION_RETAIN);
+    }
+    objc_setAssociatedObject(self, &yq_kActionHandlerPinchBlockKey, block, OBJC_ASSOCIATION_COPY);
+}
+- (void)yq_handleActionForPinchGesture:(UIPinchGestureRecognizer*)gesture
+{
+    void(^block)(UIPinchGestureRecognizer *) = objc_getAssociatedObject(self, &yq_kActionHandlerPinchBlockKey);
+    if (block) block(gesture);
+}
+
+/* 拖动手势key */
+static char yq_kActionHandlerPanBlockKey;
+
+- (void)yq_whenPan:(void (^)(UIPanGestureRecognizer * _Nonnull))block {
+    UIPanGestureRecognizer *gesture = objc_getAssociatedObject(self, yq_objcAssociatedGesturekKey);
+    if (!gesture)
+    {
+        gesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(yq_handleActionForPanGesture:)];
+        [self addGestureRecognizer:gesture];
+        objc_setAssociatedObject(self, yq_objcAssociatedGesturekKey, gesture, OBJC_ASSOCIATION_RETAIN);
+    }
+    objc_setAssociatedObject(self, &yq_kActionHandlerPanBlockKey, block, OBJC_ASSOCIATION_COPY);
+}
+- (void)yq_handleActionForPanGesture:(UIPanGestureRecognizer*)gesture
+{
+    void(^block)(UIPanGestureRecognizer *) = objc_getAssociatedObject(self, &yq_kActionHandlerPanBlockKey);
+    if (block) block(gesture);
+}
+
+/* 旋转手势key */
+static char yq_kActionHandlerRotationBlockKey;
+
+- (void)yq_whenRotation:(void (^)(UIRotationGestureRecognizer * _Nonnull))block {
+    UIRotationGestureRecognizer *gesture = objc_getAssociatedObject(self, yq_objcAssociatedGesturekKey);
+    if (!gesture)
+    {
+        gesture = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(yq_handleActionForRotationGesture:)];
+        [self addGestureRecognizer:gesture];
+        objc_setAssociatedObject(self, yq_objcAssociatedGesturekKey, gesture, OBJC_ASSOCIATION_RETAIN);
+    }
+    objc_setAssociatedObject(self, &yq_kActionHandlerRotationBlockKey, block, OBJC_ASSOCIATION_COPY);
+}
+- (void)yq_handleActionForRotationGesture:(UIRotationGestureRecognizer*)gesture
+{
+    void(^block)(UIRotationGestureRecognizer *) = objc_getAssociatedObject(self, &yq_kActionHandlerRotationBlockKey);
+    if (block) block(gesture);
+}
+
 @end
+
+
+
+
+
+
+
+
